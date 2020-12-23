5f6c4428237e0e04c507ebd8b21eed8520e0af6b
njazgunovich <nicholasjaz@gmail.com>
2020-12-22 21:34:48 -0500

    api response

--- script.js
+++ script.js
@@ -1,2 +1,42 @@
 var ZomAPIKey = "19770ef1d81d900e465460e5b29f22bb";
-var SpoonAPIKey = "bcbf713ed7404d57a08dc53d07322fa1";
+var SpoonAPIKey = "9baea515a18f0180bd3d0bdb1d4a4cfc";
+var Queryurl = "https://cors-anywhere.herokuapp.com/https://api.spoonacular.com/recipes/complexSearch?query=pasta"
+
+
+
+
+
+
+$(document).ready(function() {
+    $("#search-recipe").on("click", function(event) {
+        event.preventDefault()
+        console.log("hello")
+        var typeOfFood = $("#type-of-food").val().trim()
+        test(typeOfFood)
+    })
+
+    function test(search) {
+        var edamanUrl = "https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q=" + search + "&app_id=566ccbc5&app_key=d5a7bcf8eae7b83123232dbd9a2d1b4f"
+
+        $.ajax({
+            url: edamanUrl,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response.hits)
+            var Responsearray = response.hits
+            for (var i = 0; i < Responsearray.length; i++) {
+                console.log(Responsearray[i].recipe.image)
+                var image = $("<img>")
+                image.attr("src", Responsearray[i].recipe.image)
+                console.log(image)
+                $("#cardcontainer").append(image)
+            }
+        })
+    }
+
+});
+
+
+//function typeFood() {
+//let foodImput = $("")
+//}
