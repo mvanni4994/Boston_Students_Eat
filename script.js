7bc3cdad9a5a5f2ae604b08222191ccb3237d0d3
lindsey Bowen <lindseyjbowen@gmail.com>
2020-12-29 14:40:12 -0500

    chnaged title/button color

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+var instance = M.Parallax.getInstance(elems);
+function elems(){
+
+}
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.parallax');
     var instances = M.Parallax.init(elems, options);
   });
+
+ 
