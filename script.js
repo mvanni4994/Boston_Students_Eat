20e2878b2ab5b7e64408a6240057ee4c6a553774
njazgunovich <nicholasjaz@gmail.com>
2021-01-02 16:02:34 -0500

    this a commit on saturday jan 2nd

--- script.js
+++ script.js
@@ -15,7 +15,24 @@ $(document).ready(function() {
         test(typeOfFood)
     })
 
+    var newHtml = `<div class="tile is-ancestor">
+        <div class="tile is-vertical is-8">
+            <div class="tile">
+                <div class="tile is-parent">
+                    <article class="tile is-child notification is-info">
+                        <p class="title">Middle tile</p>
+                        <p class="subtitle">With an image</p>
+                        <figure class="image is-4by3">
+                            <img src="https://bulma.io/images/placeholders/640x480.png">
+                        </figure>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </div>`
+
     function test(search) {
+        $("#cardcontainer").empty()
         var edamanUrl = "https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q=" + search + "&app_id=566ccbc5&app_key=d5a7bcf8eae7b83123232dbd9a2d1b4f"
 
         $.ajax({
@@ -26,10 +43,44 @@ $(document).ready(function() {
             var Responsearray = response.hits
             for (var i = 0; i < Responsearray.length; i++) {
                 console.log(Responsearray[i].recipe.image)
-                var image = $("<img>")
-                image.attr("src", Responsearray[i].recipe.image)
-                console.log(image)
-                $("#cardcontainer").append(image)
+                    // var card = $("<div>").addClass("card")
+                    // var cardImage = $('<div>').addClass("card-image")
+                    // var cardFigure = $("<figure>").addClass("image  is-128x128")
+                    // var recipeImage = $("<img>").attr("src", Responsearray[i].recipe.image)
+                    // cardFigure.append(recipeImage)
+                    // cardImage.append(cardFigure)
+                    // card.append(cardImage)
+                    // var cardContainer = $("<div>").addClass("tile")
+                    // cardContainer.append(card)
+
+                let ingredients = '';
+
+                Responsearray[i].recipe.ingredientLines.forEach((item) => {
+                    ingredients += '<li>' + item + '</li>' + '\n'
+                })
+
+                var card = `<div class="tile is-3">
+                        <div class="tile">
+                            <div class="tile is-parent">
+                                <article class="tile is-child notification is-info">
+                                    <p class="title">${Responsearray[i].recipe.label}</p>
+                                    <figure class="image is-4by3">
+                                        <img src="${Responsearray[i].recipe.image}">
+                                    </figure>
+                                    <p class="subtitle mt-2">The Ingridents</p>
+                                    <ul>
+                                    ${ingredients}
+                                    </ul>
+                                </article>
+                            </div>
+                        </div>
+                    </div>`
+                $("#cardcontainer").append(card)
+                    // var url = Responsearray[i].recipe.url
+                    // var image = $("<img>")
+                    // image.attr("src", Responsearray[i].recipe.image)
+                    // console.log(image)
+                    // $("#cardcontainer").append(image)
             }
         })
     }
@@ -38,5 +89,7 @@ $(document).ready(function() {
 
 
 //function typeFood() {
+//let foodImput = $("")
+//}unction typeFood() {
 //let foodImput = $("")
 //}
