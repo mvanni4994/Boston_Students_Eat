9722b2e307aba2b8eb7fc04a4417d2edb0f0c7bc
Margaret Elson <margaret.a.elson@gmail.com>
2021-01-04 21:44:10 -0500

    Styling changes made to cards and a loader is added

--- NEscript.js
+++ NEscript.js
@@ -79,7 +79,7 @@ function takeout() {
 
                 var menuUrl = response.restaurants[i].restaurant.menu_url;
                 console.log(menuUrl)
-                var menuLink = ($("<a>").attr("href", menuUrl).text("Click here for more info on the restaurant!"));
+                var menuLink = ($("<a>").attr("href", menuUrl).text("Click here for more info on the restaurant!").addClass("link"));
                 card.append(menuLink); 
 
                 var cardBreak = $("<br>");
